fe0be0b12805bcfae9f25792d9104990f1cc1e8f
Alex Gornov <zmokizmoghi@gmail.com>
2015-01-12 12:44:41 +0800

    Add validation for accounts amount

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,22 +1,37 @@
 class Account < ActiveRecord::Base
   validates_presence_of :account_type
+  #validates_presence_of :accountable_type
+  #validates_presence_of :accountable_id
+
   validate :check_account_type
+  validate :check_accounts_amount
+
   before_save :normalize_account_type
-  #belongs_to :team
-  #belongs_to :user
+
   belongs_to :accountable, polymorphic: true
   has_many :transactions
 
   private
 
-  def normalize_account_type
-    self.account_type = self.account_type.downcase
-  end
-
-  def check_account_type
-    allowed_types = %w(balance payment)
-    unless allowed_types.include? account_type.downcase
-      errors.add(:account_type, "contains wrong argument")
+    def normalize_account_type
+      self.account_type = self.account_type.downcase if self.account_type.present?
     end
-  end
+
+    def check_account_type
+      return unless account_type
+      allowed_types = %w(balance payment)
+      unless allowed_types.include? account_type.downcase
+        errors.add(:account_type, "contains wrong argument")
+      end
+    end
+
+    def check_accounts_amount
+      return unless accountable_id and accountable_type
+      amount = Account.where(:accountable_id => accountable_id,
+                              :accountable_type => accountable.class.name)
+                      .length
+      if amount >= 2
+        errors[:base] << "2 accounts for this parent already exists."
+      end
+    end
 end
