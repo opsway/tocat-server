967094ff81c002bff7e18ccb70951a839b12e871
Alex Gornov <zmokizmoghi@gmail.com>
2015-01-16 00:19:40 +0800

    Migrate to Rails-API gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  include ActionController::Serialization
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  #protect_from_forgery with: :null_session
 
   def default_serializer_options
     {root: false}
