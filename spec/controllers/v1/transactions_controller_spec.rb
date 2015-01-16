51e4c1f6d423c8b8eb9105abba348d0ab469daa4
Alex Gornov <zmokizmoghi@gmail.com>
2015-01-16 15:46:24 +0800

    Set proper code style for controllerSpesc

--- spec/controllers/v1/transactions_controller_spec.rb
+++ spec/controllers/v1/transactions_controller_spec.rb
@@ -1,43 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe V1::TransactionsController, type: :controller do
-
-  describe "/transaction " do
-
+  describe '/transaction ' do
     before(:each) do
-      team = create(:team)
+      create(:team)
       create_list(:transaction, 5)
       get :index, format: :json
       @body = JSON.parse(response.body)
     end
 
-    it "returns a successful 200 response" do
+    it 'returns a successful 200 response' do
       expect(response).to be_success
     end
 
-    it "should check JSON schema" do
-      expect(response).to match_response_schema_to_list("transactions")
-
+    it 'should check JSON schema' do
+      expect(response).to match_response_schema_to_list('transactions')
     end
   end
 
-  describe "/transaction/:id" do
+  describe '/transaction/:id' do
     before(:each) do
       team = create(:team)
-      get :show, id: create(:transaction, account: team.balance_account).id, format: :json
+      get :show,
+          id: create(:transaction, account: team.balance_account).id,
+          format: :json
       @body = JSON.parse(response.body)
     end
 
-    it "returns a successful 200 response" do
+    it 'returns a successful 200 response' do
       expect(response).to be_success
     end
 
-    it "should check JSON schema" do
-      expect(response).to match_response_schema("transaction")
-
+    it 'should check JSON schema' do
+      expect(response).to match_response_schema('transaction')
     end
-
   end
-
-
 end
