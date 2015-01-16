c67e8d19bc496aef6c98c26e9d0b86b6e4e4f097
Alex Gornov <zmokizmoghi@gmail.com>
2015-01-16 16:28:12 +0800

    Fix TeamSerializer

--- app/serializers/team_show_serializer.rb
+++ app/serializers/team_show_serializer.rb
@@ -1,11 +1,15 @@
 class TeamShowSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :name, :links
+  attributes :id, :name, :balance_account_state, :income_account_state
 
   private
 
-  def links
-    {}
+  def balance_account_state
+    { total: object.balance_account.balance }
+  end
+
+  def income_account_state
+    { total: object.income_account.balance }
   end
 end
