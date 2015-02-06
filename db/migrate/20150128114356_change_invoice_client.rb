7412a37418c5a1afd3dfbcde623b5e357ae5eeea
Alex Gornov <zmokizmoghi@gmail.com>
2015-02-06 23:31:12 +0800

    Fixes pt2

--- db/migrate/20150128114356_change_invoice_client.rb
+++ db/migrate/20150128114356_change_invoice_client.rb
@@ -1,7 +1,7 @@
 class ChangeInvoiceClient < ActiveRecord::Migration
   def change
     change_column :invoices, :client, :string, :null => true
-    change_column :invoices, :order_id, :string, :null => true
+    change_column :invoices, :order_id, :string, :null => true if column_exists? :invoices, :order_id
 
   end
 end
