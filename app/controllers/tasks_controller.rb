83e26ebe88fb1bd697188aac2aa8c7488099f9b0
Alex Gornov <zmokizmoghi@gmail.com>
2015-02-12 16:50:30 +0800

    Test fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -83,22 +83,22 @@ class TasksController < ApplicationController
   end
 
   def set_budgets
-    errors = {}
+    invalid_record = nil
     params[:budget].each do |record|
       db_record = TaskOrders.where(task_id: @task.id, order_id: record[1]['order_id']).first
       if db_record.present?
         db_record.budget = record[1]['budget']
-        errors[record[1]['order_id']] = db_record.errors[:base] unless db_record.save
+        invalid_record = db_record unless db_record.save
       else
         new_db_record = @task.task_orders.new order_id: record[1]['order_id'],
                                               budget: record[1]['budget']
-        errors[record[1]['order_id']] = new_db_record.errors[:base] unless new_db_record.save
+        invalid_record = new_db_record unless new_db_record.save
       end
     end
-    if errors.empty?
+    if invalid_record.nil?
       render json: {}, status: 200
     else
-      render json: errors, status: 406
+      render json: error_builder(invalid_record, 'TASK'), status: 422
     end
   end
 
