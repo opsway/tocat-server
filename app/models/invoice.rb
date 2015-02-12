d96364dec548a9f9f8b2e0094a5dd2b4507f6bd2
Alex Gornov <zmokizmoghi@gmail.com>
2015-02-13 00:47:13 +0800

    paid callbacks

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,13 +1,18 @@
 class Invoice < ActiveRecord::Base
   validates_presence_of :external_id
-  belongs_to :order
+  has_many :orders
 
   before_destroy :check_if_invoice_used
+  after_save :handle_paid_status, if: Proc.new { |o| o.paid_changed?}
 
   private
 
+  def handle_paid_status
+    orders.each { |o| o.update_attributes(paid: paid)}
+  end
+
   def check_if_invoice_used
-    if order.present?
+    if orders.present?
       errors[:base] << 'Invoice is linked to orders'
       return false
     end
