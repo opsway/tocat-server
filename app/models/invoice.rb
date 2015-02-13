de48168a7a9e6a32ece66f9617afdf9a3688b2a4
Alex Gornov <zmokizmoghi@gmail.com>
2015-02-13 22:21:12 +0800

    Refactor paid update and wrap paid updates to transaction

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -6,15 +6,23 @@ class Invoice < ActiveRecord::Base
   before_save :handle_paid_status, if: Proc.new { |o| o.paid_changed?}
 
   private
-
   def handle_paid_status
-    orders.each { |o| o.update_attributes(paid: paid)}
+    self.transaction do
+      orders.each do |order|
+        order.tasks.each {|task| task.handle_paid(paid)}
+        raise ActiveRecord::Rollback unless order.handle_paid(paid)
+        order.sub_orders.each do |sub_order|
+          sub_order.tasks.each {|task| task.handle_paid(paid)}
+          raise ActiveRecord::Rollback unless sub_order.handle_paid(paid)
+        end
+      end
+    end
   end
 
   def check_if_invoice_used
     if orders.present?
       errors[:base] << 'Invoice is linked to orders'
-      return false
+      false
     end
   end
 end
