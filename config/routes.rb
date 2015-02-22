a06355d53388aec46661d8a6bc2bbe1d6e9b13a7
Alex Gornov <zmokizmoghi@gmail.com>
2015-02-22 19:25:13 +0800

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,99 +1,99 @@
 Rails.application.routes.draw do
   resources :order,
             controller: 'orders',
-            path: 'order',
+            path: 'orders',
             defaults: { format: 'json' }
 
-  post '/order/:id/invoice',
+  post '/orders/:id/invoice',
        to: 'orders#set_invoice',
        as: 'set_invoice',
        format: 'json'
 
-  delete '/order/:id/invoice',
+  delete '/orders/:id/invoice',
          to: 'orders#delete_invoice',
          as: 'delete_invoice',
          format: 'json'
 
-  get '/order/:id/suborder',
+  get '/orders/:id/suborder',
       to: 'orders#suborders',
       as: 'suborders',
       format: 'json'
 
-  post '/order/:id/suborder',
+  post '/orders/:id/suborder',
        to: 'orders#create_suborder',
        as: 'new_suborder',
        format: 'json'
 
   resources :team,
             controller: 'teams',
-            path: 'team',
+            path: 'teams',
             defaults: { format: 'json' },
             only: [:index, :show]
 
   resources :transaction,
             controller: 'transactions',
-            path: 'transaction',
+            path: 'transactions',
             defaults: { format: 'json' },
             only: [:index, :show]
 
   resources :user,
             controller: 'users',
-            path: 'user',
+            path: 'users',
             defaults: { format: 'json' },
             only: [:index, :show]
 
   resources :task,
             controller: 'tasks',
-            path: 'task',
+            path: 'tasks',
             defaults: { format: 'json' },
             only: [:index, :create, :show]
 
-  post '/task/:id/accept',
+  post '/tasks/:id/accept',
        to: 'tasks#set_accepted',
        as: 'task_set_accepted',
        format: 'json'
 
-  delete '/task/:id/accept',
+  delete '/tasks/:id/accept',
          to: 'tasks#delete_accepted',
          as: 'task_remove_accepted',
          format: 'json'
 
-  post '/task/:id/resolver',
+  post '/tasks/:id/resolver',
        to: 'tasks#set_resolver',
        as: 'task_set_resolver',
        format: 'json'
 
-  delete '/task/:id/resolver',
+  delete '/tasks/:id/resolver',
          to: 'tasks#delete_resolver',
          as: 'task_remove_resolver',
          format: 'json'
 
-  get '/task/:id/budget',
+  get '/tasks/:id/budget',
       to: 'tasks#budgets',
       as: 'task_get_budget',
       format: 'json'
 
-  post '/task/:id/budget',
+  post '/tasks/:id/budget',
        to: 'tasks#set_budgets',
        as: 'task_set_budget',
        format: 'json'
 
-  get '/task/:id/order',
+  get '/tasks/:id/orders',
       to: 'tasks#orders',
       as: 'task_get_orders',
       format: 'json'
 
   resources :invoice,
             controller: 'invoices',
-            path: 'invoice',
+            path: 'invoices',
             defaults: { format: 'json' }
 
-  post '/invoice/:id/paid',
+  post '/invoices/:id/paid',
        to: 'invoices#set_paid',
        as: 'invoice_set_paid',
        format: 'json'
 
-  delete '/invoice/:id/paid',
+  delete '/invoices/:id/paid',
          to: 'invoices#delete_paid',
          as: 'invoice_remove_paid',
          format: 'json'
