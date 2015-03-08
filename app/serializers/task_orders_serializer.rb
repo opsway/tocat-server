fd4ce3b706d1586c33b1ccee7b5215787ce37673
Alex Gornov <zmokizmoghi@gmail.com>
2015-03-08 15:27:16 +0700

    Add orderName to task index output

--- app/serializers/task_orders_serializer.rb
+++ app/serializers/task_orders_serializer.rb
@@ -1,9 +1,13 @@
 class TaskOrdersSerializer < ActiveModel::Serializer
-  attributes :order_id, :budget
+  attributes :order_id, :budget, :order_name
 
   private
 
   def budget
     object.budget.to_f
+  end
+
+  def order_name
+    object.order.name
   end
 end
