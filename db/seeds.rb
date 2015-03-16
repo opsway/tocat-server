0016b13628b7ae57ce92e030d9c4bda462a025d7
Alex Gornov <zmokizmoghi@gmail.com>
2015-03-16 15:45:46 +0700

    Comment seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,42 +24,42 @@
 # end
 
 
-team_list = ['OpsWay1', 'OpsWay2', 'OpsWay3', 'OpsWay4', 'OpsWay5', 'Central Office']
-team_list.each {|name| Team.create( name: name) }
-
-role_list = %W(Manager Developer)
-role_list.each {|name| Role.create( name: name) }
-
-user_list = [
-  [ "Dmitriy Ivanenko",       62,   'dmiva', 'OpsWay1', 'Developer' ],
-  [ "Aleksandr Ishchenko",    27,   'altis', 'OpsWay1', 'Developer' ],
-  [ "Volodymir Rudakov",      38,   'vorud', 'OpsWay2', 'Developer' ],
-  [ "Stanislav Derebcinschi", 52,   'stder', 'OpsWay2', 'Developer' ],
-  [ "Yuriy Kobrynyuk",        39,   'yukob', 'OpsWay2', 'Developer' ],
-  [ "Stanislav Pivovartsev",  67,   'stpiv', 'OpsWay2', 'Developer' ],
-  [ "Oksana Melnik",          69,   'okmel', 'OpsWay2', 'Manager' ],
-  [ "Andrey Lebedinskiy",     78,   'anleb', 'OpsWay3', 'Manager' ],
-  [ "Sergiy Morin",            0,   'morin.sergey1', 'OpsWay2', 'Manager' ],
-  [ "Yurii Lunhol",           31,   'yulun', 'OpsWay2', 'Developer' ],
-  [ "Ruslan Abdullaev",       62,   'ruabd', 'OpsWay2', 'Developer' ],
-  [ "Nikita Kushnir",         52,   'nikus', 'OpsWay2', 'Developer' ],
-  [ "Stas Morgun",            26,   'stmor', 'OpsWay3', 'Developer'],
-  [ "Peter Kurbatsky",        31,   'pekub', 'OpsWay4', 'Developer' ],
-  [ "Max Voronov",            57,   'mavor', 'OpsWay1', 'Developer' ],
-  [ "Dmitrii Mikhailov",      49,   'dimih', 'OpsWay1', 'Developer' ],
-  [ "Andriy Zherebchenko",    98,   'anzhe', 'OpsWay1', 'Developer' ],
-  [ "Alexandr Vronskiy",      104,  'alvro', 'OpsWay1', 'Developer' ],
-  [ "Alexander Dmitrienko",   72,   'aldmi', 'OpsWay1', 'Developer' ],
-  [ "Alexander Gornov",       69,   'algor', 'OpsWay3', 'Developer' ],
-  [ "Sergey Gorchakov",       52,   'segor', 'OpsWay1', 'Manager' ],
-  [ "Andriy Samilyak",        00,   'verlgoff', 'OpsWay4', 'Manager' ]
-
-]
-
-
-user_list.each do |name, rate, login, team, role|
-  User.create( name: name, daily_rate: rate, login: login, team: Team.find_by_name(team), role: Role.find_by_name(role) )
-end
+# team_list = ['OpsWay1', 'OpsWay2', 'OpsWay3', 'OpsWay4', 'OpsWay5', 'Central Office']
+# team_list.each {|name| Team.create( name: name) }
+#
+# role_list = %W(Manager Developer)
+# role_list.each {|name| Role.create( name: name) }
+#
+# user_list = [
+#   [ "Dmitriy Ivanenko",       62,   'dmiva', 'OpsWay1', 'Developer' ],
+#   [ "Aleksandr Ishchenko",    27,   'altis', 'OpsWay1', 'Developer' ],
+#   [ "Volodymir Rudakov",      38,   'vorud', 'OpsWay2', 'Developer' ],
+#   [ "Stanislav Derebcinschi", 52,   'stder', 'OpsWay2', 'Developer' ],
+#   [ "Yuriy Kobrynyuk",        39,   'yukob', 'OpsWay2', 'Developer' ],
+#   [ "Stanislav Pivovartsev",  67,   'stpiv', 'OpsWay2', 'Developer' ],
+#   [ "Oksana Melnik",          69,   'okmel', 'OpsWay2', 'Manager' ],
+#   [ "Andrey Lebedinskiy",     78,   'anleb', 'OpsWay3', 'Manager' ],
+#   [ "Sergiy Morin",            0,   'morin.sergey1', 'OpsWay2', 'Manager' ],
+#   [ "Yurii Lunhol",           31,   'yulun', 'OpsWay2', 'Developer' ],
+#   [ "Ruslan Abdullaev",       62,   'ruabd', 'OpsWay2', 'Developer' ],
+#   [ "Nikita Kushnir",         52,   'nikus', 'OpsWay2', 'Developer' ],
+#   [ "Stas Morgun",            26,   'stmor', 'OpsWay3', 'Developer'],
+#   [ "Peter Kurbatsky",        31,   'pekub', 'OpsWay4', 'Developer' ],
+#   [ "Max Voronov",            57,   'mavor', 'OpsWay1', 'Developer' ],
+#   [ "Dmitrii Mikhailov",      49,   'dimih', 'OpsWay1', 'Developer' ],
+#   [ "Andriy Zherebchenko",    98,   'anzhe', 'OpsWay1', 'Developer' ],
+#   [ "Alexandr Vronskiy",      104,  'alvro', 'OpsWay1', 'Developer' ],
+#   [ "Alexander Dmitrienko",   72,   'aldmi', 'OpsWay1', 'Developer' ],
+#   [ "Alexander Gornov",       69,   'algor', 'OpsWay3', 'Developer' ],
+#   [ "Sergey Gorchakov",       52,   'segor', 'OpsWay1', 'Manager' ],
+#   [ "Andriy Samilyak",        00,   'verlgoff', 'OpsWay4', 'Manager' ]
+#
+# ]
+#
+#
+# user_list.each do |name, rate, login, team, role|
+#   User.create( name: name, daily_rate: rate, login: login, team: Team.find_by_name(team), role: Role.find_by_name(role) )
+# end
 
 #
 # 3.times do
