fd550f2097a6e7dee0c23b6aec745e17de4fbf31
Andriy Samilyak <samilyak@gmail.com>
2015-03-24 09:30:27 +0200

    Seed data

--- db/import.sql
+++ db/import.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 truncate accounts;
 truncate task_orders;
 truncate tasks;
@@ -7,25 +6,17 @@ truncate users;
 truncate teams;
 truncate roles;
 
-=======
-truncate users;
-truncate teams;
-truncate roles;
->>>>>>> 0eebebbe23faf1744efb6860e2f7d96061acc7be
 insert into roles values (1, "Manager", now(), now());
 insert into roles values (2, "Developer", now(), now());
 insert into teams values (1, "OpsWay1", now(), now());
 insert into teams values (2, "OpsWay2", now(), now());
 insert into teams values (3, "OpsWay3", now(), now());
-<<<<<<< HEAD
 
-=======
->>>>>>> 0eebebbe23faf1744efb6860e2f7d96061acc7be
 insert into users values(1, "Dev1", 'dev1', 1, 50, now(), now(), 2);
 insert into users values(2, "Dev2", 'dev2', 2, 60, now(), now(), 2);
 insert into users values(3, "Dev3", 'dev3', 3, 70, now(), now(), 2);
 insert into users values(4, "Dev4", 'dev4', 3, 80, now(), now(), 2);
-<<<<<<< HEAD
+
 
 insert into accounts values(1, 'balance', now(), now(), 1, 'Team');
 insert into accounts values(2, 'balance', now(), now(), 2, 'Team');
@@ -41,5 +32,4 @@ insert into accounts values(11, 'payment', now(), now(), 1, 'User');
 insert into accounts values(12, 'payment', now(), now(), 2, 'User');
 insert into accounts values(13, 'payment', now(), now(), 3, 'User');
 insert into accounts values(14, 'payment', now(), now(), 4, 'User');
-=======
->>>>>>> 0eebebbe23faf1744efb6860e2f7d96061acc7be
+
