15db5df1c64947d9b7d05e204d1116b627382a28
Andriy Samilyak <samilyak@gmail.com>
2015-03-28 15:02:51 +0200

    Test update

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ls spec/ | awk '{print $1}' | grep _spec.js | xargs -I % sh -c '{ echo %; /usr/bin/jasmine-node --junitreport /var/www/current/spec/%; sleep 1; }'
+ls /var/www/current/spec/ | awk '{print $1}' | grep _spec.js | xargs -I % sh -c '{ echo %; /usr/bin/jasmine-node --junitreport /var/www/current/spec/%; sleep 1; }'
