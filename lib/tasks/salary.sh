5ca69b09280795e53245b8e158f83b97bb82ffe4
Alex Gornov <zmokizmoghi@gmail.com>
2015-04-21 01:32:07 +0700

    //

--- lib/tasks/salary.sh
+++ lib/tasks/salary.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd cd /var/www/current
+cd /var/www/current
 source /usr/local/rvm/rubies/ruby-2.1.1/bin/ruby
-rake RAILS_ENV=production shiftplanning:test
+rake RAILS_ENV=production shiftplanning:update_transactions
