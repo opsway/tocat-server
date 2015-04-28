cc6b59880d4028a82290ee08537441493c78314d
Alex Gornov <zmokizmoghi@gmail.com>
2015-04-28 19:39:37 +0700

    temporary disable transaction check

--- lib/self_check.rb
+++ lib/self_check.rb
@@ -20,9 +20,9 @@ class SelfCheck
     messages << accepted_and_paid_for_teams
     messages << orders_complete_flag
     messages << task_uniqness
-    Transaction.where.not(id: @transactions.join(',')).where.not('comment LIKE "%salary%"').each do |transaction|
-      messages << "Transaction ##{transaction.id}: #{transaction.comment} wrong!"
-    end
+    # Transaction.where.not(id: @transactions.join(',')).where.not('comment LIKE "%salary%"').each do |transaction|
+    #   messages << "Transaction ##{transaction.id}: #{transaction.comment} wrong!"
+    # end
     messages.flatten!
   end
 
