fd4ab449fda40b003b43a9dd316136e052e37091
Andriy Samilyak <samilyak@gmail.com>
2015-05-20 16:08:27 +0300

    Update tests on #26875

---
+++ spec/task_paid_with_order_different_team_spec.js
@@ -0,0 +1,30 @@
+var config = require('./config');
+var url = config.url;
+
+frisby.create('Correct invoice')
+    .post(url + '/invoices',
+    {
+        "external_id": '67899000000303011' + Math.random()
+    })
+    .expectStatus(201)
+    .afterJSON(function(invoice){
+        
+              frisby.create('Invoice order id=1 with correct invoice. Order is hardcoded in import.sql')
+                .post(url + '/order/1' +  + '/invoice', {'invoice_id' : invoice.id})
+                .expectStatus(200)
+                .toss();
+
+              frisby.create('Set invoice paid')
+                .post(url + '/invoice/' + invoice.id + '/paid')
+                .expectStatus(200)
+                .toss();
+    
+              frisby.create('Check that order is paid')
+                .get(url + '/order/1')
+                .expectStatus(200)
+                .expectJSON({'paid' : true})
+                .toss();
+
+            //TODO: Check that team2 tx created, check that user1 tx created
+    })
+    .toss();
