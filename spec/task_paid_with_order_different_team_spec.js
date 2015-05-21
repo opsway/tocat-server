ceeab7390b2bf8e2478d2c004676862bb8b6a34c
Andriy Samilyak <samilyak@gmail.com>
2015-05-21 13:00:50 +0300

    Test update

--- spec/task_paid_with_order_different_team_spec.js
+++ spec/task_paid_with_order_different_team_spec.js
@@ -4,13 +4,13 @@ var url = config.url;
 frisby.create('Correct invoice')
     .post(url + '/invoices',
     {
-        "external_id": '67899000000303011' + Math.random()
+        "external_id": '67899000000303011' + Math.floor(Math.random() * 1000000)
     })
     .expectStatus(201)
     .afterJSON(function(invoice){
         
               frisby.create('Invoice order id=1 with correct invoice. Order is hardcoded in import.sql')
-                .post(url + '/order/1' +  + '/invoice', {'invoice_id' : invoice.id})
+                .post(url + '/order/1/invoice', {'invoice_id' : invoice.id})
                 .expectStatus(200)
                 .toss();
 
@@ -22,6 +22,13 @@ frisby.create('Correct invoice')
               frisby.create('Check that order is paid')
                 .get(url + '/order/1')
                 .expectStatus(200)
+                .expectJSON({'paid' : true})
+                .toss();
+            
+              frisby.create('Expecting task 1 (hardcoded) to be paid')
+                .get(url + '/task/1')
+                .expectStatus(200)
+                .inspectBody()
                 .expectJSON({'paid' : true})
                 .toss();
 
