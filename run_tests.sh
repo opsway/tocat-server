9fced885c687468693b9b6875a5036b6f2a6397b
Alex Gornov <zmokizmoghi@gmail.com>
2015-05-28 13:33:58 +0700

    reset test runner

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ls /var/www/current/spec/ | awk '{print $1}' | grep _spec.js | xargs -I % sh -c '{ echo %; cd /var/www/current && RAILS_ENV=production bundle exec rake sql_data:load && /usr/bin/jasmine-node --junitreport /var/www/current/spec/%; sleep 1; }'
+ls /var/www/current/spec/ | awk '{print $1}' | grep _spec.js | xargs -I % sh -c '{ echo %; /usr/bin/jasmine-node --junitreport /var/www/current/spec/%; sleep 1; }'
