18e96e414ea8d97480690d4ae918851238208a31
Alex Gornov <zmokizmoghi@gmail.com>
2015-05-28 07:42:57 +0700

    fix selfcheck

--- lib/self_check.rb
+++ lib/self_check.rb
@@ -41,13 +41,12 @@ class SelfCheck
     messages.flatten!
   end
 
-  #private
+  private
 
   def check_invoices
     messages = []
     RedmineTocatApi.get_invoices.each do |record|
       record.symbolize_keys!
-      binding.pry
       invoice = Invoice.where(external_id: record[:invoice_id]).first
       if invoice.present?
         if record[:currency_code] == "USD"
@@ -58,7 +57,6 @@ class SelfCheck
           messages << "Invoice #{invoice.external_id}(#{record[:invoice_number]} in zoho) has invalid paid status." if invoice.paid != status
         else
           messages << "Invoice #{invoice.external_id}(#{record[:invoice_number]} in zoho) has invalid total: It has #{invoice.total}, but it should be #{record[:total] * record[:exchange_rate]}." if invoice.total != (record[:total] * record[:exchange_rate])
-          #messages << "Invoice #{invoice.external_id}(#{record[:invoice_number]} in zoho) cannot be proceed: it has total in #{record[:currency_code]}"
         end
       end
     end
@@ -441,7 +439,6 @@ class SelfCheck
           if calculated_budget < 0
             messages << "Expecting order #{order.id} (#{order.name}) free budget to be greater than zero"
           end
-          #binding.pry if order.id == 43
           if val =! (order.free_budget + order.allocatable_budget)
             messages << "Order #{order.id} (#{order.name}) has invalid free budget!"
           end
