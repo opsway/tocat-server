6499f1e37b2c8afa5e7e915ac12834b442784553
Alex Gornov <zmokizmoghi@gmail.com>
2015-08-01 20:32:58 +0700

    Fix: task spec now green

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it { should validate_presence_of(:external_id) }
+  it { should validate_presence_of(:external_id).with_message("Missing external task ID") }
   it { should have_many(:orders).through(:task_orders) }
   it { should belong_to(:user) }
 end
