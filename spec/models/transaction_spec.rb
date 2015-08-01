e68b429bd59564e2312d18dfb913f74d5d1666c7
Alex Gornov <zmokizmoghi@gmail.com>
2015-08-01 20:33:59 +0700

    Fix: transaction spec now green

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -4,6 +4,5 @@ RSpec.describe Transaction, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:account) }
   it { should validate_presence_of(:account_id) }
-  it { should validate_presence_of(:user_id) }
   it { should validate_presence_of(:total) }
 end
