b29dd1f8f9fffdbbb42e08ff7e6398a06279b4b4
Andriy Samilyak <samilyak@gmail.com>
2015-11-15 16:14:51 +0200

    Correcting test run command

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #ls /var/www/current/spec/ | awk '{print $1}' | grep _spec.js | xargs -I % sh -c '{ echo %; /usr/bin/jasmine-node --junitreport /var/www/current/spec/%; sleep 1; }'
 cd /var/www/current
-RAILS_ENV=production /usr/local/rvm/gems/ruby-2.1.1/wrappers/rake test:internal
+RAILS_ENV=production /usr/local/rvm/gems/ruby-2.1.1@global/wrappers/bundle exec rake test:internal
