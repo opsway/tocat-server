6059f16bbc1414b8ba9851df3ba4879578d63578
vlaew <vlaew5@gmail.com>
2016-02-26 12:00:11 +0300

    Fix test

--- spec/tasks/set_resolver_spec.js
+++ spec/tasks/set_resolver_spec.js
@@ -7,17 +7,10 @@ frisby.create('Correct task creation')
     .post(url + '/tasks', {"external_id": + task01 })
     .expectStatus(201)
     .afterJSON(function(task){
-        frisby.create('Get user manager')
-            .get(url + '/users', { "search": "role = \"Manager\"" })
-            .expectStatus(200)
-            .afterJSON(function(users){
-                frisby.create('Set manager as a resolver of a task should fail')
-                    .post(url + '/task/' + task.id + '/resolver', { 'user_id': users[0].id })
-                    .expectStatus(422)
-                    .expectJSON({errors:['Manager can not be set as a resolver']})
-                    .toss();
-            })
+        frisby.create('Set manager as a resolver of a task should fail')
+            .post(url + '/task/' + task.id + '/resolver', { 'user_id': 8 })
+            .expectStatus(422)
+            .expectJSON({errors:['Manager can not be set as a resolver']})
             .toss();
     })
     .toss();
-
