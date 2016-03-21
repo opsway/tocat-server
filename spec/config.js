0869d574255579829108093acbc5246990f0de6c
vlaew <vlaew5@gmail.com>
2016-03-22 01:18:29 +0300

    Remove local debug code

--- spec/config.js
+++ spec/config.js
@@ -1,5 +1,4 @@
-//exports.url = 'http://test.tocat.opsway.com';
-exports.url = 'http://localhost:3005';
+exports.url = 'http://test.tocat.opsway.com';
 frisby = require('frisby');
 
 frisby.globalSetup({
