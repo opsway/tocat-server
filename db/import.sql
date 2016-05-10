f943f9911738fe9abab4298aa4b373dc37fb1cab
vlaew <vlaew5@gmail.com>
2016-05-10 13:18:08 +0300

    Fix import.sql

--- db/import.sql
+++ db/import.sql
@@ -20,17 +20,17 @@ insert into teams values (2, "OpsWay2", now(), now(), 40,4);
 insert into teams values (3, "OpsWay3", now(), now(), 40,4);
 insert into teams values (4, "Central Office", now(), now(), 40,4);
 
-insert into users values(1, "Dev1", 'dev1', 1, 50, now(), now(), 2, true, true, 'dev1@opsway.com');
-insert into users values(2, "Dev2", 'dev2', 2, 60, now(), now(), 2, true, true, 'dev2@opsway.com');
-insert into users values(5, "Dev5", 'dev5', 2, 61, now(), now(), 2, true, true, 'dev5@opsway.com');
-insert into users values(3, "Dev3", 'dev3', 3, 70, now(), now(), 2, true, true, 'dev3@opsway.com');
-insert into users values(4, "Dev4", 'dev4', 3, 80, now(), now(), 2, true, true, 'dev4@opsway.com');
-insert into users values(6, "Dev6", 'dev6', 1, 40, now(), now(), 2, true, true, 'dev6@opsway.com');
-insert into users values(7, "inactive", 'inactive', 1, 40, now(), now(), 2, false, true, 'inactive@opsway.com');
-insert into users values(8, "Manager1", 'man1', 1, 40, now(), now(), 1, true, true, 'man1@opsway.com');
-insert into users values(9, "Manager2", 'man2', 2, 40, now(), now(), 1, true, true, 'man2@opsway.com');
-insert into users values(10, "Manager3", 'man3', 3, 40, now(), now(), 1, true, true, 'man3@opsway.com');
-insert into users values(11, "Manager4", 'man4', 4, 40, now(), now(), 1, true, true, 'man4@opsway.com');
+insert into users values(1, "Dev1", 'dev1', 1, 50, now(), now(), 2, true, 'dev1@opsway.com', true);
+insert into users values(2, "Dev2", 'dev2', 2, 60, now(), now(), 2, true, 'dev2@opsway.com', true);
+insert into users values(5, "Dev5", 'dev5', 2, 61, now(), now(), 2, true, 'dev5@opsway.com', true);
+insert into users values(3, "Dev3", 'dev3', 3, 70, now(), now(), 2, true, 'dev3@opsway.com', true);
+insert into users values(4, "Dev4", 'dev4', 3, 80, now(), now(), 2, true, 'dev4@opsway.com', true);
+insert into users values(6, "Dev6", 'dev6', 1, 40, now(), now(), 2, true, 'dev6@opsway.com', true);
+insert into users values(7, "inactive", 'inactive', 1, 40, now(), now(), 2, false, 'inactive@opsway.com', true);
+insert into users values(8, "Manager1", 'man1', 1, 40, now(), now(), 1, true, 'man1@opsway.com', true);
+insert into users values(9, "Manager2", 'man2', 2, 40, now(), now(), 1, true, 'man2@opsway.com', true);
+insert into users values(10, "Manager3", 'man3', 3, 40, now(), now(), 1, true, 'man3@opsway.com', true);
+insert into users values(11, "Manager4", 'man4', 4, 40, now(), now(), 1, true, 'man4@opsway.com', true);
 
 insert into accounts values(1, 'balance', now(), now(), 1, 'Team');
 insert into accounts values(2, 'balance', now(), now(), 2, 'Team');
