b5f2ba61922147c357cecf4c2f82890f6516fd15
Vitaliy <vbykovec@gmail.com>
2016-05-12 12:59:56 +0300

    update import.sql for deploy to test server

--- db/import.sql
+++ db/import.sql
@@ -20,17 +20,17 @@ insert into teams values (2, "OpsWay2", now(), now(), 40,4);
 insert into teams values (3, "OpsWay3", now(), now(), 40,4);
 insert into teams values (4, "Central Office", now(), now(), 40,4);
 
-insert into users values(1, "Dev1", 'dev1', 1, 50, now(), now(), 2, true);
-insert into users values(2, "Dev2", 'dev2', 2, 60, now(), now(), 2, true);
-insert into users values(5, "Dev5", 'dev5', 2, 61, now(), now(), 2, true);
-insert into users values(3, "Dev3", 'dev3', 3, 70, now(), now(), 2, true);
-insert into users values(4, "Dev4", 'dev4', 3, 80, now(), now(), 2, true);
-insert into users values(6, "Dev6", 'dev6', 1, 40, now(), now(), 2, true);
-insert into users values(7, "inactive", 'inactive', 1, 40, now(), now(), 2, false);
-insert into users values(8, "Manager1", 'man1', 1, 40, now(), now(), 1, true);
-insert into users values(9, "Manager2", 'man2', 2, 40, now(), now(), 1, true);
-insert into users values(10, "Manager3", 'man3', 3, 40, now(), now(), 1, true);
-insert into users values(11, "Manager4", 'man4', 4, 40, now(), now(), 1, true);
+insert into users(id, name, login, team_id, daily_rate, created_at, updated_at, role_id, active) values(1, "Dev1", 'dev1', 1, 50, now(), now(), 2, true);
+insert into users(id, name, login, team_id, daily_rate, created_at, updated_at, role_id, active) values(2, "Dev2", 'dev2', 2, 60, now(), now(), 2, true);
+insert into users(id, name, login, team_id, daily_rate, created_at, updated_at, role_id, active) values(5, "Dev5", 'dev5', 2, 61, now(), now(), 2, true);
+insert into users(id, name, login, team_id, daily_rate, created_at, updated_at, role_id, active) values(3, "Dev3", 'dev3', 3, 70, now(), now(), 2, true);
+insert into users(id, name, login, team_id, daily_rate, created_at, updated_at, role_id, active) values(4, "Dev4", 'dev4', 3, 80, now(), now(), 2, true);
+insert into users(id, name, login, team_id, daily_rate, created_at, updated_at, role_id, active) values(6, "Dev6", 'dev6', 1, 40, now(), now(), 2, true);
+insert into users(id, name, login, team_id, daily_rate, created_at, updated_at, role_id, active) values(7, "inactive", 'inactive', 1, 40, now(), now(), 2, false);
+insert into users(id, name, login, team_id, daily_rate, created_at, updated_at, role_id, active) values(8, "Manager1", 'man1', 1, 40, now(), now(), 1, true);
+insert into users(id, name, login, team_id, daily_rate, created_at, updated_at, role_id, active) values(9, "Manager2", 'man2', 2, 40, now(), now(), 1, true);
+insert into users(id, name, login, team_id, daily_rate, created_at, updated_at, role_id, active) values(10, "Manager3", 'man3', 3, 40, now(), now(), 1, true);
+insert into users(id, name, login, team_id, daily_rate, created_at, updated_at, role_id, active) values(11, "Manager4", 'man4', 4, 40, now(), now(), 1, true);
 
 insert into accounts values(1, 'balance', now(), now(), 1, 'Team');
 insert into accounts values(2, 'balance', now(), now(), 2, 'Team');
