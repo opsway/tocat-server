1b3ce6bbe6d9034d935449d76fd9afca7e2907e9
Vitaliy <vbykovec@gmail.com>
2016-06-08 10:14:26 +0300

    update transaction comments migration

--- db/migrate/20160608064135_update_transaction_comments.rb
+++ db/migrate/20160608064135_update_transaction_comments.rb
@@ -2,13 +2,17 @@ class UpdateTransactionComments < ActiveRecord::Migration
   def change
     Transaction.where("comment like 'Accepted and paid issue%'").find_each do |t|
       task = Task.find_by_external_id(t.comment.split.last)
-      t.comment = "Accepted and paid issue #{task.id}"
-      t.save
+      if task
+        t.comment = "Accepted and paid issue #{task.id}"
+        t.save
+      end
     end
     Transaction.where("comment like 'Reopening issue%'").find_each do |t|
       task = Task.find_by_external_id(t.comment.split.last)
-      t.comment = "Reopening issue #{task.id}"
-      t.save
+      if task
+        t.comment = "Reopening issue #{task.id}"
+        t.save
+      end
     end
   end
 end
