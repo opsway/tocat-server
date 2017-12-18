82d0b66a129e9f9a29c508dfba8d32ff8cf94a55
Denys Kulyk <morequalified@gmail.com>
2017-12-18 16:36:05 +0200

    transactions visibility

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,13 +2,21 @@ class TransactionsController < ApplicationController
   before_action :set_transaction, except: [:index]
 
   def index
-    transactions = Transaction.includes(:account).search_for(params[:search])
-    if params[:user].present?
+    user = User.current_user
+    @articles = Transaction.includes(:account).search_for(params[:search]).order(sort)
+    if params[:user].present? && (user.manager? || user.coach?)
       account_name = Account.find(params[:user]).name
       account_ids = Account.where(name: account_name).pluck :id
-      transactions = transactions.where(account_id: account_ids)
+      @articles = @articles.where(account_id: account_ids)
+    else
+      if user.manager?
+        team_members_ids = User.where(team_id: user.team.id).ids
+        @articles = @articles.where(user_id: team_members_ids)
+      elsif !user.manager? && !user.coach?
+        user_accounts = Account.where(accountable_id: user.id).pluck :id
+        @articles = @articles.where(account_id: user_accounts)
+      end
     end
-    @articles = transactions.order(sort)
     paginate json: @articles, per_page: params[:limit]
   end
 
